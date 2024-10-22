c81a638ef176055d9a72135aca97fbb20288ebf7
Philani Ncube <ncbphi001@gmail.com>
2024-10-22 15:34:44 +0200

    Refactor wine loading component to update filter layout and add loading skeletons

--- app/(public)/wines/loading.tsx
+++ app/(public)/wines/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 const Loading = () => {
   return (
-    <div className='flex flex-col gap-6 md:flex-row'>
+    <div className='flex flex-col gap-6 md:flex-row container'>
       <div className='max-w-[200px]'>
         <h2 className='text-lg font-semibold'>Filter</h2>
         <div className="mt-4 bg-slate-300">
@@ -14,7 +14,7 @@ const Loading = () => {
         </div>
 
       </div>
-      <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3'>
+      <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 flex-1'>
         <Skeleton className='aspect-square w-full bg-slate-300' />
         <Skeleton className='aspect-square w-full bg-slate-300' />
         <Skeleton className='aspect-square w-full bg-slate-300' />
