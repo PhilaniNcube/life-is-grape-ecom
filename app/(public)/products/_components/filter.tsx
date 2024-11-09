4fd4b42e96f4e5311f8c6834ab5d0650d5c00282
Philani Ncube <ncbphi001@gmail.com>
2024-11-09 18:03:25 +0200

    Enhance product filter component with scroll area and improved styling for better usability

--- app/(public)/products/_components/filter.tsx
+++ app/(public)/products/_components/filter.tsx
@@ -1,3 +1,5 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import Link from "next/link";
@@ -8,9 +10,9 @@ const Filter = async () => {
 
   console.log(JSON.stringify(categories, null, 2))
 
-  return <div className="w-full">
+  return <div className="w-full pr-3">
     <h2 className="text-lg font-semibold text-gray-800">Filter</h2>
-    <div className="mt-4">
+    <ScrollArea className="mt-4 w-full h-[400px]">
       {categories.map(category => {
 
         // if category has a parent_id it is a child category so we skip it
@@ -21,28 +23,29 @@ const Filter = async () => {
 
 
         return (
-          <div key={category.name}>
+          <div key={category.name} className="w-full flex flex-col">
             <Link
               href={`/products?category=${category.slug}`}
-              className='text-md font-semibold text-gray-800'
+              className='text-md font-semibold text-gray-800 w-full px-2 py-2 hover:bg-slate-300 rounded'
             >
               {category.name}
             </Link>
-            <ul className='mt-2 flex flex-col space-y-2'>
+            <ul className='mt-2 flex flex-col px-5'>
               {category.children.map((child) => (
                 <Link
                   href={`/products?category=${child.slug}`}
                   key={child._id}
-                  className='text-sm text-gray-600'
+                  className='text-sm text-gray-600 mb-1'
                 >
                   {child.name}
                 </Link>
               ))}
+
             </ul>
           </div>
         )
       })}
-    </div>
+    </ScrollArea>
     </div>;
 };
 export default Filter;
