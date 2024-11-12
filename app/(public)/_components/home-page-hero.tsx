7fc3d637a540c387854551e6665828db01ec7a12
Philani Ncube <ncbphi001@gmail.com>
2024-11-12 19:00:00 +0200

    Update HomePageHero component with enhanced layout and styling for improved visual appeal

--- app/(public)/_components/home-page-hero.tsx
+++ app/(public)/_components/home-page-hero.tsx
@@ -3,40 +3,32 @@ import Image from "next/image";
 
 const HomePageHero = () => {
   return (
-    <section className='relative flex h-[50vh] items-center justify-center overflow-hidden lg:h-[60vh]'>
+    <section className='relative flex h-[80vh] items-center justify-center overflow-hidden lg:h-[75vh]'>
       <Image
-        src='https://quiet-caterpillar-834.convex.cloud/api/storage/be2c45fa-5b0d-4076-8b23-fb0199a89d83'
+        src='https://quiet-caterpillar-834.convex.cloud/api/storage/c42d1e73-5c2e-426c-8937-9e10fc9efe37'
         alt='Elegant wine glass in a vineyard setting'
         width={1920}
-        height={1080}
+        height={1400}
         className='absolute inset-0 h-full w-full object-cover'
         priority
       />
-      <div className='absolute inset-0 bg-opacity-50' />
-      <div className='relative z-10 px-4 text-center sm:px-6 lg:px-8'>
-        <h1 className='mb-4 text-2xl font-bold text-black sm:text-4xl lg:text-6xl'>
-          Explore Our Diverse Collection of Wines and Spirits
+      <div className='absolute inset-0 bg-white lg:bg-transparent' />
+      <div className='container relative z-10 px-4 sm:px-6 lg:px-8'>
+        <h1 className='mb-4 w-[20ch] text-2xl font-bold text-black sm:text-4xl lg:text-6xl'>
+          Explore Our Diverse Collection of{' '}
+          <span className='font-extrabold text-purple-900'>Wines</span> and{' '}
+          <span className="text-red-700">Spirits</span>
         </h1>
-        <p className='mx-auto mb-8 max-w-3xl text-lg text-gray-200 sm:text-xl'>
+        <p className='mb-8 max-w-[30ch] text-lg sm:text-xl'>
           Discover a wide range of fine wines and spirits tailored to develop
           and satisfy every palate.
         </p>
-        <Button className='transform rounded-full px-8 py-3 text-lg text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-red-800'>
+        <Button
+          size='lg'
+          className='transform rounded-full px-8 py-3 text-lg text-white transition duration-300 ease-in-out hover:bg-slate-800 lg:min-w-[300px]'
+        >
           Shop Now
         </Button>
-      </div>
-      <div className='absolute bottom-4 left-1/2 -translate-x-1/2 transform'>
-        <svg
-          className='h-6 w-6 animate-bounce text-white'
-          fill='none'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeWidth='2'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-        >
-          <path d='M19 14l-7 7m0 0l-7-7m7 7V3'></path>
-        </svg>
       </div>
     </section>
   )
