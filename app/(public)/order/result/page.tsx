e66f6d265e9a07c3c04ff30f62c9b9f4cb9aa95b
Philani Ncube <ncbphi001@gmail.com>
2024-11-27 10:59:39 +0200

    Refactor PaymentCallbackPage to use async props for search parameters

--- app/(public)/order/result/page.tsx
+++ app/(public)/order/result/page.tsx
@@ -1,15 +1,13 @@
 import { Suspense } from 'react'
 import { PaymentStatus } from '../_components/payment-status'
 
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
 
-export default function PaymentCallbackPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
+
+export default async function PaymentCallbackPage(props:{searchParams:SearchParams}) {
+  const searchParams  = await props.searchParams
   // In a real-world scenario, you would verify the payment status with your payment gateway here
-  const paymentStatus =
-    searchParams.status === 'success' ? 'success' : 'failure'
+  const paymentStatus = searchParams.status === 'success' ? 'success' : 'failure'
 
   return (
     <div className='container mx-auto px-4 py-8'>
