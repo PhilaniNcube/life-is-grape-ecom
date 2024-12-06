3de881b9e0efceaf13a530c6654b8f99650c7334
Philani Ncube <ncbphi001@gmail.com>
2024-12-06 13:14:09 +0200

    Refactor HomePageHero component to use Link for navigation and improve button styling

--- app/(public)/_components/home-page-hero.tsx
+++ app/(public)/_components/home-page-hero.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const HomePageHero = () => {
   return (
@@ -17,20 +18,22 @@ const HomePageHero = () => {
         <h1 className='mb-4 w-[20ch] text-2xl font-bold text-black sm:text-4xl lg:text-6xl'>
           Explore Our Diverse Collection of{' '}
           <span className='font-extrabold text-purple-900'>Wines</span> and{' '}
-          <span className="text-red-700">Spirits</span>
+          <span className='text-red-700'>Spirits</span>
         </h1>
         <p className='mb-8 max-w-[30ch] text-lg sm:text-xl'>
           Discover a wide range of fine wines and spirits tailored to develop
           and satisfy every palate.
         </p>
-        <Button
-          size='lg'
-          className='transform rounded-full px-8 py-3 text-lg text-white transition duration-300 ease-in-out hover:bg-slate-800 lg:min-w-[300px]'
-        >
-          Shop Now
-        </Button>
+        <Link href='/products'>
+          <Button
+            size='lg'
+            className='transform rounded-none px-8 py-3 text-lg text-white transition duration-300 ease-in-out hover:bg-slate-800 lg:min-w-[300px]'
+          >
+            Shop Now
+          </Button>
+        </Link>
       </div>
     </section>
   )
-};
-export default HomePageHero;
+}
+export default HomePageHero
