ecbc60bfb2b80fbbbc8523acb8343e1f120480dd
Philani Ncube <ncbphi001@gmail.com>
2025-01-17 17:49:19 +0200

    Remove GiftBoxes component from Home page for cleaner layout

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       {/* <WinesSection /> */}
       {/* <Brands /> */}
       <AboutUs />
-      <GiftBoxes />
+      {/* <GiftBoxes /> */}
       <SugarbirdBanner />
     </section>
   )
