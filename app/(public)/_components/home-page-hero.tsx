373fcec4ba9c2af8b3807cc740bf5c1734abea4b
Philani Ncube <ncbphi001@gmail.com>
2025-01-17 17:45:01 +0200

    Refactor HomePageHero component: restructure layout for better responsiveness and enhance typography

--- app/(public)/_components/home-page-hero.tsx
+++ app/(public)/_components/home-page-hero.tsx
@@ -6,39 +6,43 @@ import Link from 'next/link'
 
 const HomePageHero = () => {
   return (
-    <section className='relative flex h-[80vh] items-center justify-center overflow-hidden lg:h-[75vh]'>
-      <Image
-        src='https://quiet-caterpillar-834.convex.cloud/api/storage/fab54c25-b425-4a60-b930-9fbe5c41a8c3'
-        alt='Elegant wine glass in a vineyard setting'
-        width={1920}
-        height={1400}
-        className='absolute inset-0 h-full w-full object-cover'
-        priority
-      />
-      <div className='absolute inset-0 bg-white/40 lg:bg-transparent' />
-      <div className='container relative z-10 px-4 sm:px-6 lg:px-8'>
-        <h1
-          className={cn(
-            'mb-4 w-[20ch] text-5xl font-bold lowercase text-slate-700 sm:text-4xl lg:text-6xl'
-          , littlepot.className)}
-        >
-          We Make
-          <br />
-          grape descisions
-        </h1>
-        <p className='mb-8 max-w-[23ch] text-sm'>
-          Discover a wide range of fine wines and spirits tailored to develop
-          and satisfy every palate.
-        </p>
-        <Link href='/products'>
-          <Button
-            size='lg'
-            className='transform rounded-none px-8 py-3 text-lg text-white bg-gray-700 transition duration-300 ease-in-out hover:bg-slate-800 lg:min-w-[300px]'
+    <section className='relative flex h-[80vh] overflow-hidden lg:h-[75vh]'>
+      <div className='mx-auto grid container lg:grid-cols-3'>
+        <div className='relative z-10 flex flex-col justify-center col-span-1'>
+          <h1
+            className={cn(
+              'mb-4 text-5xl font-bold lowercase text-slate-700 sm:text-4xl lg:text-6xl',
+              littlepot.className
+            )}
           >
-            Shop Now
-          </Button>
-        </Link>
+            We Make
+            <br />
+            grape descisions
+          </h1>
+          <p className='mb-8 max-w-[44ch] text-sm'>
+            Discover a wide range of fine wines and spirits tailored to develop
+            and satisfy every palate.
+          </p>
+          <Link href='/products'>
+            <Button
+              size='lg'
+              className='transform rounded-none bg-gray-700 px-8 py-3 text-lg text-white transition duration-300 ease-in-out hover:bg-slate-800 lg:min-w-[300px]'
+            >
+              Shop Now
+            </Button>
+          </Link>
+        </div>{' '}
+        <Image
+          src='https://quiet-caterpillar-834.convex.cloud/api/storage/fab54c25-b425-4a60-b930-9fbe5c41a8c3'
+          alt='Elegant wine glass in a vineyard setting'
+          width={1920}
+          height={1400}
+          className='h-full w-full object-cover object-right hidden lg:flex lg:col-span-2'
+          priority
+        />
       </div>
+
+      <div className='absolute inset-0 bg-white/40 lg:bg-transparent' />
     </section>
   )
 }
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   return (
     <section className=''>
       <HomePageHero />
-      {/* <GrapeDescisions /> */}
+
       <DeliveryInfo />
-      {/* <HomePageGrid /> */}
+      <HomePageGrid />
       {/* <WinesSection /> */}
       {/* <Brands /> */}
       <AboutUs />
