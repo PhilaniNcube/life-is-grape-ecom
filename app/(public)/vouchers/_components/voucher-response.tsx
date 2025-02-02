ead042b9b4b6b61329cc7fae6f710d461438fdfc
Philani Ncube <ncbphi001@gmail.com>
2025-02-02 20:43:18 +0200

    Refactor VoucherResponse component layout and improve payment status display

--- app/(public)/vouchers/_components/voucher-response.tsx
+++ app/(public)/vouchers/_components/voucher-response.tsx
@@ -14,31 +14,35 @@ const VoucherResponse = async ({id}:{id:Id<"gift_vouchers">}) => {
 
   return (
     <div className='container mx-auto py-10'>
-      <h1 className={cn('mb-4 text-2xl font-bold', littlepot.className)}>Payment Status</h1>
+      <h1 className={cn('mb-4 text-2xl font-bold', littlepot.className)}>
+        Payment Status
+      </h1>
       {voucher && (
-        <div className="grid md:grid-cols-2 gap-6">
-        <div>
-          <h2 className='mb-4 text-lg font-bold'>Gift Voucher</h2>
-          <p>
-            <span className='font-bold'>Recipient:</span>{' '}
-            {voucher.recipient_email}
-          </p>
-          <p>
-            <span className='font-bold'>Value:</span>{' '}
-            {formatPrice(voucher.value)}
-          </p>
-          <p>
-            <span className='font-bold'>Voucher:</span> {voucher.code}
-          </p>
-        </div>
-        <div>
-          <h2 className='mb-4 text-lg font-bold'>Payment Status</h2>
-          <p>
-            <span className='font-bold'>Status:</span> {voucher.paid}
-          </p>
-          <p>
-            <span className='font-bold'>Redeemed:</span> {voucher.redeemed ? 'Yes' : 'No'}
-          </p>
+        <div className='grid gap-6 md:grid-cols-2'>
+          <div>
+            <h2 className='mb-4 text-lg font-bold'>Gift Voucher</h2>
+            <p>
+              <span className='font-bold'>Recipient:</span>{' '}
+              {voucher.recipient_email}
+            </p>
+            <p>
+              <span className='font-bold'>Value:</span>{' '}
+              {formatPrice(voucher.value)}
+            </p>
+            <p>
+              <span className='font-bold'>Voucher:</span> {voucher.code}
+            </p>
+          </div>
+          <div>
+            <h2 className='mb-4 text-lg font-bold'>Payment Status</h2>
+            <p>
+              <span className='font-bold'>Status:</span>{' '}
+              {voucher.paid ? 'Paid' : 'Not Paid'}
+            </p>
+            <p>
+              <span className='font-bold'>Redeemed:</span>{' '}
+              {voucher.redeemed ? 'Yes' : 'No'}
+            </p>
           </div>
         </div>
       )}
