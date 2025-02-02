c37e685d9f4f03c9831f0acc39fdeb4510351505
Philani Ncube <ncbphi001@gmail.com>
2025-02-02 22:29:39 +0200

    Add ConfirmVoucherPayment component to display voucher confirmation

--- app/(public)/gift-voucher/[id]/page.tsx
+++ app/(public)/gift-voucher/[id]/page.tsx
@@ -1,9 +1,12 @@
 import { Id } from "@/convex/_generated/dataModel";
+import ConfirmVoucherPayment from "../_components/confirm-voucher-payment";
 
 const page = async ({params}:{params:Promise<{id:Id<"gift_vouchers">}>}) => {
 
   const { id } = await params;
 
-  return <div>pages</div>;
+  return <div>
+    <ConfirmVoucherPayment id={id} />
+  </div>;
 };
 export default page;
--- app/(public)/gift-voucher/_components/confirm-voucher-payment.tsx
+++ app/(public)/gift-voucher/_components/confirm-voucher-payment.tsx
@@ -1,17 +1,23 @@
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
-import { fetchQuery } from "convex/nextjs";
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { fetchQuery } from 'convex/nextjs'
+import GiftVoucherConfirmation from './confirm'
 
-const ConfirmVoucherPayment = async ({id}:{id:Id<"gift_vouchers">}) => {
+const ConfirmVoucherPayment = async ({ id }: { id: Id<'gift_vouchers'> }) => {
+  const voucher = await fetchQuery(api.gift_vouchers.getGiftVoucher, { id })
 
-  const voucher = await fetchQuery(api.gift_vouchers.getGiftVoucher, { id });
-
-  if(!voucher) {
-    return <div className="mx-auto container">
-      <h1 className="text-2xl font-bold">Voucher not found</h1>
-    </div>;
+  if (!voucher) {
+    return (
+      <div className='container mx-auto'>
+        <h1 className='text-2xl font-bold'>Voucher not found</h1>
+      </div>
+    )
   }
 
-  return <div>ConfirmVoucherPayment</div>;
-};
-export default ConfirmVoucherPayment;
+  return (
+    <div>
+      <GiftVoucherConfirmation voucher={voucher} />
+    </div>
+  )
+}
+export default ConfirmVoucherPayment
