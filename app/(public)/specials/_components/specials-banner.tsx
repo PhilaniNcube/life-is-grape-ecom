be1d59855d97160ddef8f92526e750ff39ff4036
Philani Ncube <ncbphi001@gmail.com>
2025-02-03 11:53:45 +0200

    Add SpecialsBanner component with Valentine's theme and update link

--- app/(public)/specials/_components/specials-banner.tsx
+++ app/(public)/specials/_components/specials-banner.tsx
@@ -1,8 +1,7 @@
-import { littlepot } from '@/app/fonts'
-import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
-import Image from 'next/image'
-import Link from 'next/link'
+import { littlepot } from "@/app/fonts";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 const SpecialsBanner = () => {
   return (
@@ -16,18 +15,19 @@ const SpecialsBanner = () => {
       />
       <div className='absolute inset-0 flex items-center justify-center'>
         <div className='container'>
-          <h1 className={cn('text-4xl font-bold sm:text-5xl lg:text-6xl', littlepot.className)}>
-          Would you be my Valentine?
+          <h1
+            className={cn(
+              'text-4xl font-bold sm:text-5xl lg:text-6xl',
+              littlepot.className
+            )}
+          >
+            Would you be my Valentine?
           </h1>
           <p className='mt-4 text-xl'>The perfect gift for your loved one</p>
-          <Link href='/specials'>
-            <Button className='mt-3 w-[200px] rounded-none bg-red-700 text-white'>
-              Wines
-            </Button>
-          </Link>
+
         </div>
       </div>
     </section>
   )
-}
-export default SpecialsBanner
+};
+export default SpecialsBanner;
