8a215a1c9faa1db4ed7022f2f866b3a7d866bdfd
Philani Ncube <ncbphi001@gmail.com>
2025-02-19 16:59:19 +0200

    Fetch and display dynamic banner content in SpecialsBanner component

--- app/(public)/_components/specials-banner.tsx
+++ app/(public)/_components/specials-banner.tsx
@@ -1,19 +1,30 @@
 import { littlepot } from '@/app/fonts'
 import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
 import { cn } from '@/lib/utils'
+import { fetchQuery } from 'convex/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const SpecialsBanner = () => {
+const SpecialsBanner = async () => {
+
+  const banner =await  fetchQuery(api.banner.getBanner)
+
+  if (!banner) return
+
+
   return (
     <section className='relative mt-6'>
-      <Image
-        src='https://fabulous-peacock-233.convex.cloud/api/storage/8c74297f-099e-4518-b87b-547dff692e60'
-        alt='Sugarbird Fynbos Gin'
-        width={1920}
-        height={1080}
-        className='max-h-[55vh] w-full object-cover'
-      />
+      {banner.image && (
+        <Image
+          src={banner.image}
+          alt='Sugarbird Fynbos Gin'
+          width={1920}
+          height={1080}
+          className='max-h-[55vh] w-full object-cover aspect-video'
+        />
+      )}
+     
       <div className='absolute inset-0 flex items-center justify-center'>
         <div className='container'>
           <h1
@@ -22,10 +33,10 @@ const SpecialsBanner = () => {
               littlepot.className
             )}
           >
-            Be my Valentine
+         {banner.title}
           </h1>
-          <p className='mt-4 text-xl'>The perfect gift for your loved one</p>
-          <Link href='/specials'>
+          <p className='mt-4 text-xl'>{banner.subtitle}</p>
+          <Link href={banner.link}>
             <Button className='mt-3 w-[200px] rounded-none bg-red-700 text-white'>
               Shop
             </Button>
