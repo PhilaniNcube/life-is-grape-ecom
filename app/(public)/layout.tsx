464f8e76e52d094423c749446479688c09845f99
Philani Ncube <ncbphi001@gmail.com>
2025-03-08 11:22:51 +0200

    Update Next.js to 15.2.1 and add Google Tag Manager component for enhanced tracking

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -4,6 +4,7 @@ import { CartStoreProvider } from '@/store/cart-store-provider'
 import AgeVerificationBanner from '@/components/age-verification'
 import ComingSoon from '@/components/coming-soon'
 import { Toaster } from '@/components/ui/sonner'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default async function PublicLayout({
   children,
@@ -12,13 +13,13 @@ export default async function PublicLayout({
 }>) {
   return (
     <CartStoreProvider>
+      <GoogleTagManager gtmId='GTM-56G5XSV8' />
       {/* <ComingSoon />   */}
       <div className='flex h-[100dvh] flex-col bg-white'>
         <PublicHeader />
         <main className='grow'>
           <AgeVerificationBanner />
           {children}
-
         </main>
         <Toaster />
         <Footer />
